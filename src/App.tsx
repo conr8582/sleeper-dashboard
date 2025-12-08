c7c2efc023d1cd519ac20f9f97f9897c14366703
conr8582 <barry@Barrys-MacBook-Pro.local>
2025-12-08 13:42:03 -0500

    Initial commit from correct project folder

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  return (
+    <div
+    style={{
+      padding: '2rem',
+      fontFamily:
+      'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", sans-serif',
+    }}>
+      <h1>Sleeper Dashboard</h1>
+      <p>Hello World - first version.</p>
+    </div>
+  );
+}
+
+export default App;
